10d2b4e63c4c2422b3fc2c95f2f529ffa96aa110
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-22 16:47:45 -0600

    Add the variables and objects exercise

---
+++ objects.rb
@@ -0,0 +1,14 @@
+name1 = "larry"
+health1 = 60
+
+name2 = "curly"
+health2 = 125
+puts "#{name2.upcase} has a health of #{health2}."
+
+name3 = "moe"
+health3 = 100
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
+
+name4 = "shemp"
+health4 = 90
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
