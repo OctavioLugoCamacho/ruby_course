a033b539bc574f55aca965625bf6e1e8fcf283dc
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-23 18:26:40 -0600

    Add the main files for the exercises

---
+++ studio_game.rb
@@ -0,0 +1,12 @@
+require_relative 'game'
+require_relative 'player'
+
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
