24f096adb046c4da9797af76840f004fbd563a6b
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-29 10:06:43 -0600

    Add the methods for the movie 3d class

--- separate_files.rb
+++ separate_files.rb
@@ -1,4 +1,5 @@
 require_relative 'movie'
+require_relative 'movie3d'
 require_relative 'playlist'
 
 movie1 = Movie.new("star wars", 10)
@@ -7,6 +8,8 @@ movie3 = Movie.new("avengers")
 
 playlist1 = Playlist.new("Octavio")
 playlist1.load(ARGV.shift || "movies.csv")
+movie3d = Movie3D.new('revenge of the sith', 5, 20)
+playlist1.add_movie(movie3d)
 
 loop do
   puts "How many viewings? ('quit' to exit)"
