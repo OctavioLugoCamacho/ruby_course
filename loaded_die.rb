4fd45a56eff7cf22efb568cd4c3f5c6cb35307c2
Octavio Lugo Camacho <72223559+OctavioLugoCamacho@users.noreply.github.com>
2022-03-29 13:52:02 -0600

    Add the new modules for the game

---
+++ loaded_die.rb
@@ -0,0 +1,14 @@
+require_relative 'auditable'
+
+class LoadedDie
+  include Auditable
+
+  attr_reader :number
+
+  def roll
+    numbers = [1, 1, 2, 5, 6, 6]
+    @number = numbers.sample
+    audit
+    @number
+  end
+end
